d8e38f157f14c337d0c2c08851075918399e9354
pisiliang <2282561186@qq.com>
2023-06-07 17:19:56 +0800

    提交

--- test_vue/src/components/HelloWorld.vue
+++ test_vue/src/components/HelloWorld.vue
@@ -2,9 +2,8 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      点击进入斗鱼网站
+      <a href="https://www.douyu.com/" target="_blank" rel="noopener">斗鱼</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
