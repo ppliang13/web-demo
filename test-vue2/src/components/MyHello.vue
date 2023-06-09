2644152a2aadf1bac6ee9b109714944120c0fe35
pisiliang <2282561186@qq.com>
2023-06-09 09:34:24 +0800

    提交

---
+++ test-vue2/src/components/MyHello.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+  {{ message }}
+  </div>
+</template>
+
+<script>
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+</script>
+
+<style>
+
+</style>
